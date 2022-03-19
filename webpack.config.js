2c759a01ad979d8dbca23835e702d9ac8ec4cee6
Siva Nagi Reddy Alla <siva.alla@outlook.com>
2022-03-19 23:07:12 +0530

    Add base project setup for react app development

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+
+module.exports = {
+    entry: './src/index.js',
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                loader: "babel-loader",
+                options: {presets: ["@babel/env"]}
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ["*", ".js", ".jsx"]
+    },
+    output: {
+        path: path.resolve(__dirname, "dist/"),
+        publicPath: "/dist/",
+        filename: "bundle.js"
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        port: 3000,
+        allowedHosts: [
+          'localhost'
+        ],
+        hot: true
+    },
+    plugins: [
+    ]
+}
